dc5333a02c542228484c6a7bc99bcf58cacddaa0
Christopher Linnett <chrislinnett45389@gmail.com>
2026-04-11 07:55:24 +0800

    fix(build): unwrap String? overlay ids before passing to Pigeon FlutterApi

--- ios/apple_maps_flutter/Sources/apple_maps_flutter/Utils/TouchHandler.swift
+++ ios/apple_maps_flutter/Sources/apple_maps_flutter/Utils/TouchHandler.swift
@@ -17,18 +17,21 @@ class TouchHandler {
         var didOverlayConsumeTapEvent: Bool = false
         for overlay: MKOverlay in overlays {
             if let flutterPolyline: FlutterPolyline = overlay as?  FlutterPolyline {
-                if  flutterPolyline.isConsumingTapEvents ?? false && flutterPolyline.contains(coordinate: coord, mapView: view) {
-                    flutterApi?.onPolylineTap(polylineId: flutterPolyline.id, completion: pigeonLogOnError)
+                if  flutterPolyline.isConsumingTapEvents ?? false && flutterPolyline.contains(coordinate: coord, mapView: view),
+                    let id = flutterPolyline.id {
+                    flutterApi?.onPolylineTap(polylineId: id, completion: pigeonLogOnError)
                     didOverlayConsumeTapEvent = true
                 }
             } else if let flutterPolygon: FlutterPolygon = overlay as?  FlutterPolygon {
-                if  flutterPolygon.isConsumingTapEvents ?? false && flutterPolygon.contains(coordinate: coord) {
-                    flutterApi?.onPolygonTap(polygonId: flutterPolygon.id, completion: pigeonLogOnError)
+                if  flutterPolygon.isConsumingTapEvents ?? false && flutterPolygon.contains(coordinate: coord),
+                    let id = flutterPolygon.id {
+                    flutterApi?.onPolygonTap(polygonId: id, completion: pigeonLogOnError)
                     didOverlayConsumeTapEvent = true
                 }
             } else if let flutterCircle: FlutterCircle = overlay as?  FlutterCircle {
-                if  flutterCircle.isConsumingTapEvents ?? false && flutterCircle.contains(coordinate: coord) {
-                    flutterApi?.onCircleTap(circleId: flutterCircle.id, completion: pigeonLogOnError)
+                if  flutterCircle.isConsumingTapEvents ?? false && flutterCircle.contains(coordinate: coord),
+                    let id = flutterCircle.id {
+                    flutterApi?.onCircleTap(circleId: id, completion: pigeonLogOnError)
                     didOverlayConsumeTapEvent = true
                 }
             }
